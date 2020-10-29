4afb0b91d9dc9d0e715e8e40a1f8aee845409294
Corey Burkett <coreyburkett22@gmail.com>
2020-10-29 16:15:46 -0700

    Added text to navbar buttons

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -43,12 +43,12 @@ function Navbar() {
       <ul class="navbar-nav ml-auto nav-flex-icons" style={{ right: 0 }}>
             <li class="nav-item">
                 <a class="nav-link waves-effect waves-light" href="https://github.com/cburkett22" target="_blank" rel="noopener noreferrer">
-                    <i class="fab fa-github"></i>
+                    GitHub |<i class="fab fa-github"></i>
                 </a>
             </li>
             <li class="nav-item">
                 <a class="nav-link waves-effect waves-light" href="https://www.linkedin.com/in/corey-burkett/" target="_blank" rel="noopener noreferrer">
-                    <i class="fab fa-linkedin"></i>
+                    LinkedIn |<i class="fab fa-linkedin"></i>
                 </a>
             </li>
         </ul>
